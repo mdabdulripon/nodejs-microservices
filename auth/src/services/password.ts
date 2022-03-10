0f5b9904d6914d6fbd09aecddc5f1984ba9a1ae9
Abdul Ripon <mdabdulripon@gmail.com>
2022-03-10 15:06:46 -0600

    feat(hashing password)

---
+++ auth/src/services/password.ts
@@ -0,0 +1,34 @@
+import { scrypt, randomBytes } from 'crypto'
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+    static async toHash(password: string) {
+        const salt = randomBytes(8).toString('hex');
+        // * Hashing the Password *
+        const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+        return `${buf.toString('hex')}.${salt}`;
+    }
+
+    static async compare(storedPassword: string, suppliedPassword: string) {
+        // * Get the hashPassword and salt form the storedPassword *
+        const [hashPassword, salt] = storedPassword.split(',');
+
+        // * Hashing the supply Password *
+        const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+        return buf.toString('hex') === hashPassword;
+    }
+}
+
+/**
+ * TODO: https://nodejs.org/api/documentation.html
+ * ! Buffers Module
+ * ! randomBytes
+ * ! toString,
+ * ! Hex, UTF8,
+ * ! Util Module
+ * ! promisify
+ * ***/ 
