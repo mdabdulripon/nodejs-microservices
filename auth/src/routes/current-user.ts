6bdfba710c42d46535060ddc40fb0f4d20f0c093
Abdul Ripon <mdabdulripon@gmail.com>
2022-03-12 18:02:02 -0600

    feat(add current user route handelar)

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import jwt  from "jsonwebtoken";
 const router = express.Router();
 
 router.get('/api/users/currentUser', (req, res) => {
-    res.send('Current User!');
+
+    // Check if the jwt is exits
+    if (!req.session?.jwt) {
+        return res.send({ currentUser: null });
+    }
+
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+        res.send({ currentUser: payload });
+    } catch (error) {
+        res.send({ currentUser: null }); 
+    }
 });
 
 export { router as currentUserRouter };
