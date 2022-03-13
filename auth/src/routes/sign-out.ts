3bebc43ad9a715281d3e4b8939a0ad81b6a7e9da
Abdul Ripon <mdabdulripon@gmail.com>
2022-03-12 18:06:34 -0600

    feat(add sign out user route handelar)

--- auth/src/routes/sign-out.ts
+++ auth/src/routes/sign-out.ts
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 
 router.post('/api/users/signOut', (req, res) => {
-    res.send('Sign Out!!')
+    req.session = null;
+    res.send({})
 });
 
 export { router as signOutRouter };
