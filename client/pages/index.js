6f1f31d500602e9b068031090597d5bbeaafff14
Abdul Ripon <mdabdulripon@gmail.com>
2022-04-04 00:19:53 -0500

    feat(fetch current user through getInitialProps)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,23 @@
+import axios from 'axios';
 
-export default () => {
+const LandingPage = ({currentUser}) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>
 }
+
+// All the server-side rendering logic is placed here at getInitialProps
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        /**
+         * * we are in the server!
+         * * node js does not have window object. Only browser does 
+         * */  
+        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentUser',{ headers: req.headers});
+        return data;
+    } else {
+        const { data } = await axios.get('/api/users/currentUser');
+        return data;
+    }
+}
+
+export default LandingPage;
